317be0a96a76bf235c339867cede77781cd7835d
chahat <chahat.jain0@gmail.com>
2017-09-03 11:47:12 +0530

    enterAlways appBar and setting icon remove

--- app/src/main/java/com/chahat/moviedom/activity/MainActivity.java
+++ app/src/main/java/com/chahat/moviedom/activity/MainActivity.java
@@ -78,9 +78,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
