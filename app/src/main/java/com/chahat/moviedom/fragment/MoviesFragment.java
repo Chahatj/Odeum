698923b24bd251a7188d2f21a862dac130d72499
chahat <chahat.jain0@gmail.com>
2018-03-20 12:36:16 +0530

    apk release 0.0.5

--- app/src/main/java/com/chahat/moviedom/fragment/MoviesFragment.java
+++ app/src/main/java/com/chahat/moviedom/fragment/MoviesFragment.java
@@ -29,9 +29,7 @@ public class MoviesFragment extends Fragment {
     private TabLayout tabs;
     private CharSequence[] Titles={"NOW PLAYING","UPCOMING","POPULAR","TOP RATED"};
     private int Numboftabs = 4;
-   /* @BindView(R.id.adView)
-    AdView adView;
-*/
+
     public static MoviesFragment newInstance(){
         return new MoviesFragment();
     }
@@ -42,10 +40,6 @@ public class MoviesFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_movies,container,false);
         ButterKnife.bind(this,view);
-
-        /*AdRequest adRequest = new AdRequest.Builder()
-                .build();
-        adView.loadAd(adRequest);*/
 
         adapter =  new ViewPagerAdapter(getChildFragmentManager(),Titles,Numboftabs);
 
